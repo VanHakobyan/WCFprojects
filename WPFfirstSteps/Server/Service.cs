383f6e5c0691eba5c8ddf0e365036f90e81fd06b
VanHakobyan <van19962013@mail.ru>
2017-04-25 19:18:07 +0400

    add service :gem:

---
+++ WPFfirstSteps/Server/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Service : IContract
+    {
+        public void Say(string text)
+        {
+            Console.WriteLine($"Send messege {text}");
+        }
+    }
+}
