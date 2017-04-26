d05cd3f35bfe26bd27beafe3e945eb7cb32c7930
VanHakobyan <van19962013@mail.ru>
2017-04-26 11:32:03 +0400

    add non correct app :three:

--- Hosting/ClientWCF/Client.cs
+++ Hosting/ClientWCF/Client.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClientWCF
 {
+    [ServiceContract]
+    interface IServiceContract
+    {
+        [OperationContract]
+        double method(string s);
+
+    }
     class Client
     {
         static void Main(string[] args)
         {
+            Console.Title = "Client";
+            ChannelFactory<IServiceContract> factory = new ChannelFactory<IServiceContract>(new BasicHttpBinding(),new EndpointAddress("http://localhost:8000//ServiceWCF"));
+            IServiceContract service = factory.CreateChannel();
+            double num = service.method("double");
+            Console.WriteLine($"Double : {num}");
+
+            Console.WriteLine("Any key");
+            Console.ReadKey();
+
         }
     }
 }
