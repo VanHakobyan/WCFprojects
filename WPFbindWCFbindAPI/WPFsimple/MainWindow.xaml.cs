19dc889bd95a10f0dd20b525088ebcc14dd7577f
VanHakobyan <van19962013@mail.ru>
2017-04-27 00:06:04 +0400

    add sln wpf and api

---
+++ WPFbindWCFbindAPI/WPFsimple/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Web.Script.Serialization;
+using System.Net.Http.Formatting;
+
+namespace WPFsimple
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Person p = new Person() { Name = "Satenik", Age = 20 };
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = client.PostAsync(@"http://http://localhost:51001/api/main", p, new JsonMediaTypeFormatter()).Result;
+            string message = response.Content.ReadAsStringAsync().Result;
+
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+
+            string content = jss.Deserialize<string>(message);
+
+            textBlock.Text = content;
+        }
+    }
+}
---
+++ WPFbindWCFbindAPI/WebApp/Controllers/MainController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class MainController : ApiController
+    {
+        // GET: api/Main
+        Service1Client WCFclient = new Service1Client();
+        GET: api/Main
+        public string Get()
+        {
+            CompositeType Mytype = WCFclient.GetDataUsingDataContractAsync();
+            string result = WCFclient.GetDataAsync(5).Result;
+        
+            return result;
+        }
+        GET: api/Main/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Main
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Main/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Main/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
