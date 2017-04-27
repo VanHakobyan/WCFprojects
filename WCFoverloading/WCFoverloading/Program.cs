26f193f820346e280d129e546835ac36d4c985f7
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-27 18:14:17 +0400

    add:wolf: Program Service:wolf:

--- WCFoverloading/WCFoverloading/Program.cs
+++ WCFoverloading/WCFoverloading/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,14 @@ namespace WCFoverloading
     {
         static void Main(string[] args)
         {
+            Console.Title = "server";
+            ServiceHost host = new ServiceHost(typeof(Service), new Uri(@"http://localhost"));
+            host.AddServiceEndpoint(typeof(IInterface), new BasicHttpBinding(), "");
+            host.Open();
+
+            Console.WriteLine("opened...");
+            Console.ReadKey();
+            host.Close();
         }
     }
 }
