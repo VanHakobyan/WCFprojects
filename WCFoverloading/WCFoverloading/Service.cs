b8f4106206c0a5b113cb53d49391af251742ac21
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-27 17:56:41 +0400

    :cloud: add service :cloud:

---
+++ WCFoverloading/WCFoverloading/Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+namespace WCFoverloading
+{
+    [ServiceContract]
+    interface IInterface
+    {
+        [OperationContract(Name = "addInt")]
+        int Add(int x, int y);
+
+        [OperationContract(Name = "addDouble")]
+        double Add(double x, double y);
+
+    }
+    class Service : IInterface
+    {
+        public int Add(int x, int y)
+        {
+            Console.WriteLine(OperationContext.Current.RequestContext.RequestMessage);
+            return x + y;
+        }
+
+        public double Add(double x, double y)
+        {
+            Console.WriteLine(OperationContext.Current.RequestContext.RequestMessage);
+            return x + y;
+        }
+    }
+}
