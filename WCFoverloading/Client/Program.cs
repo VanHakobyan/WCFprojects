eae99c851fa755516208bd0ac8f74c9c3546ed79
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-27 18:17:29 +0400

    :whale:add client :whale:

--- WCFoverloading/Client/Program.cs
+++ WCFoverloading/Client/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Client
 {
+    [ServiceContract]
+    interface IInterface
+    {
+        [OperationContract(Name = "AddInt")]
+        int Add(int a, int b);
+
+        [OperationContract(Name = "AddDouble")]
+        double Add(double a, double b);
+    }
+
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Title = "CLIENT";
+
+            IInterface proxy = ChannelFactory<IInterface>.CreateChannel(
+                new BasicHttpBinding(), new EndpointAddress("http://localhost"));
+
+            using (proxy as IDisposable)
+            {
+                int i = proxy.Add(22, 63);
+                Console.WriteLine(i);
+
+                double d = proxy.Add(2.7, 9.3);
+                Console.WriteLine(d);
+            }
+
+            // Задержка.
+            Console.ReadKey();
         }
     }
 }
