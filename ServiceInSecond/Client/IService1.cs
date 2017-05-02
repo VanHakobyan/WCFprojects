338fabfbe8fabc03bbf714246cd105624dfc5f51
VanHakobyan <van19962013@mail.ru>
2017-05-02 19:27:11 +0400

    :v: add sln lessons :ok:

---
+++ ServiceInSecond/Client/IService1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfService;
+
+namespace Client
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+    }
+
+ 
+}
---
+++ ServiceInSecond/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Uri address = new Uri($"http://localhost:4000/IService1");
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress endpoint = new EndpointAddress(address);
+            ChannelFactory<IService1> Factory = new ChannelFactory<IService1>(binding, endpoint);
+            IService1 channel= Factory.CreateChannel();
+            string st = channel.GetData(2);
+            Console.WriteLine(st);
+            
+        }
+    }
+}
